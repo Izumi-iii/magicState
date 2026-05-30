bb85ab334cd06578d5dda18f4a403583a3ca5c64
Izumi-iii <depressing113@foxmail.com>
2026-05-30 15:27:49 +0800

    Initial Commit

---
+++ magicState/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  magicState
+//
+//  Created by 1-6 on 2026/5/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ magicState/magicStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  magicStateApp.swift
+//  magicState
+//
+//  Created by 1-6 on 2026/5/30.
+//
+
+import SwiftUI
+
+@main
+struct magicStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
